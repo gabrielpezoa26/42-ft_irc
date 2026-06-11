c6bd4f9ea309154bb09d7c91fa2642a92584b3f0
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2026-06-10 23:05:40 -0300

    hotfix

--- includes/SignalHandler.hpp
+++ includes/SignalHandler.hpp
@@ -6,12 +6,12 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 22:28:58 by gcesar-n          #+#    #+#             */
-/*   Updated: 2026/06/09 23:30:39 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2026/06/10 23:05:22 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SIGNALS_HPP
-#define SIGNALS_HPP
+#ifndef SIGNAL_HANDLER_HPP
+#define SIGNAL_HANDLER_HPP
 
 #include <signal.h>
 #include <string.h>
